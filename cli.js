06574ad5d1d6d6ccaa2999bac1d1ac6251e0886e
Dong Yeop Lee <dongy7@gmail.com>
2018-05-08 22:26:34 -0400

    download emotes using emote api

--- cli.js
+++ cli.js
@@ -4,12 +4,88 @@ const path = require('path')
 const chalk = require('chalk')
 const program = require('commander')
 const prompt = require('prompt')
+const ora = require('ora')
+const fetch = require('node-fetch')
+const request = require('request-promise')
+const Promise = require('bluebird')
+const writeFile = Promise.promisify(fs.writeFile)
+
 const config = require('./config')
 const connect = require('./client')
 
-const homeDir = os.homedir()
-const configDir = path.join(homeDir, config.dirName)
-const configFile = path.join(configDir, config.configFile)
+const HOME_DIR = os.homedir()
+const CONFIG_DIR = path.join(HOME_DIR, config.dirName)
+const CONFIG_FILE = path.join(CONFIG_DIR, config.configFile)
+const EMOTICON_DIR = path.join(CONFIG_DIR, config.emoticonDir)
+const IMAGES_DIR = path.join(EMOTICON_DIR, config.imagesDir)
+const API_BASE_URL = config.apiBaseURL
+const EMOTE_API_URL = `${API_BASE_URL}/channels`
+const GLOBAL_CHANNEL = config.globalChannel
+
+const getEmotes = async (channel) => {
+  const url = `${EMOTE_API_URL}/${channel}`
+  const res = await fetch(url)
+  const json = await res.json()
+  return json
+}
+
+const fetchEmotes = async (channel, emotes) => {
+  const spinner = ora(`Fetching emotes for ${channel}`)
+  spinner.start()
+  emotes.forEach(async emote => {
+    const { id, code } = emote
+    const dest = path.join(IMAGES_DIR, `${id}.png`)
+    const data = await request({
+      url: `https://static-cdn.jtvnw.net/emoticons/v1/${id}/1.0`,
+      encoding: null
+    })
+    const buf = Buffer.from(data, 'utf8')
+    await writeFile(dest, buf)
+  })
+  spinner.succeed()
+}
+
+const handleConnect = async (channel) => {
+  if (!fs.existsSync(CONFIG_DIR) || !fs.existsSync(CONFIG_FILE)) {
+    console.log(`No login credentials found. First add your oAuth token generated from ${chalk.underline('https://twitchapps.com/tmi/')}`)
+    return
+  }
+
+  fs.ensureDirSync(EMOTICON_DIR)
+  fs.ensureDirSync(IMAGES_DIR)
+
+  let spinner = ora(`Checking for emoticon api`)
+  spinner.start()
+  const failMsg = 'No emoticon api found. Emotes will be displayed as text.'
+  let api = false
+
+  try {
+    const res = await fetch(API_BASE_URL)
+    const json = await res.json()
+    if (json.status) {
+      spinner.succeed()
+      api = true
+    } else {
+      spinner.fail(failMsg)
+    }
+  } catch(e) {
+    spinner.fail(failMsg)
+  }
+
+  spinner = ora('Downloading channel emotes.')
+  spinner.start()
+
+  const globalEmotes = await getEmotes(GLOBAL_CHANNEL)
+  const channelEmotes = await getEmotes(channel)
+
+  spinner.succeed()
+
+  fetchEmotes(GLOBAL_CHANNEL, globalEmotes)
+  fetchEmotes(channel, channelEmotes)
+
+  const credentials = JSON.parse(fs.readFileSync(CONFIG_FILE))
+  connect(credentials, channel)
+}
 
 program
   .version('1.0.0')
@@ -17,25 +93,19 @@ program
 program
   .command('add <username> <token>')
   .action((username, token, cmd) => {
-    fs.ensureDirSync(configDir)
+    fs.ensureDirSync(CONFIG_DIR)
     const login = {
       username,
       token,
     }
 
-    fs.writeFileSync(configFile, JSON.stringify(login))
+    fs.writeFileSync(CONFIG_FILE, JSON.stringify(login))
   })
 
 program
   .command('connect <channel>')
   .action((channel, cmd) => {
-    if (!fs.existsSync(configDir) || !fs.existsSync(configFile)) {
-      console.log(`No login credentials found. First add your oAuth token generated from ${chalk.underline('https://twitchapps.com/tmi/')}`)
-      return
-    }
-
-    const credentials = JSON.parse(fs.readFileSync(configFile))
-    connect(credentials, channel)
+    handleConnect(channel)
   })
 
 program.parse(process.argv)
--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 config = {
   dirName: ".twitch-cli",
-  configFile: "config.json"
+  configFile: "config.json",
+  emoticonEndpoint: "https://twitchemotes.com/api_cache/v3/images.json",
+  emoticonDir: "emoticons",
+  emoticonFile: "images.json",
+  globalEmoteDir: 'global',
+  imagesDir: "cached",
+  apiBaseURL: "http://localhost:3000/api",
+  channelEndpoint: "/channels",
+  globalChannel: "twitch"
 }
 
 module.exports = config
